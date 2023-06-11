363ddd4e9791bd9aa88402e39463cee5fd30df8e
Dimon <bolilui.dmytro@gmail.com>
2023-06-11 17:27:17 +0300

    ADDED: getContactByID API function

--- models/contacts.js
+++ models/contacts.js
@@ -5,15 +5,25 @@ const contactsPath = path.join(`${__dirname}/`, 'contacts.json');
 
 const listContacts = async () => {
   try {
-    const data = await fs.readFile(contactsPath);
+    const contacts = await fs.readFile(contactsPath);
 
-    return JSON.parse(data);
+    return JSON.parse(contacts);
   } catch (error) {
     console.log(error.message);
   }
 };
 
-const getContactById = async (contactId) => {};
+const getContactById = async (contactId) => {
+  try {
+    // console.log(contactId);
+    const contacts = await listContacts();
+    const result = contacts.find((contact) => contact.id === contactId);
+
+    return result || null;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 const removeContact = async (contactId) => {};
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -16,11 +16,20 @@ router.get('/', async (req, res, next) => {
 });
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' });
+  const contactId = req.params.contactId;
+  const contact = await contactsAPI.getContactById(contactId);
+
+  res.json({
+    status: 'success',
+    code: 200,
+    data: {
+      result: contact,
+    },
+  });
 });
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' });
+  res.json({ status: 'success', code: 200, data: {} });
 });
 
 router.delete('/:contactId', async (req, res, next) => {
