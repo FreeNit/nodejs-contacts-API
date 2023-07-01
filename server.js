86aa4b67167334b7961d2443057ca09b25f0c93d
Dimon <bolilui.dmytro@gmail.com>
2023-07-01 14:03:16 +0300

    ADDED: config.js file (transferred DB_HOST string inside it)

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const app = require('./app');
 
-const DB_HOST =
-  'mongodb+srv://dmytrokovach333:jajtGw9A4ZCC2tBG@cluster0.91ksr0k.mongodb.net/db-contacts?retryWrites=true&w=majority';
+const { DB_HOST } = require('./config');
 
 mongoose
   .connect(DB_HOST)
