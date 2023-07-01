dd25461ebbe178ec5427490b32fd3f8b39e40539
Dimon <bolilui.dmytro@gmail.com>
2023-07-01 18:13:29 +0300

    Move DB_HOST to process.env

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ mongoose
     console.log('Database connected success');
     app.listen(3000, () => {
       console.log('Server running. Use our API on port: 3000');
+      console.log(process.env);
     });
   })
   .catch((error) => {
