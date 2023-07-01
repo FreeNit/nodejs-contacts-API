feeab76448289837c9fc7c72c0ca1e5232059c77
Dimon <bolilui.dmytro@gmail.com>
2023-07-01 13:12:41 +0300

    ADDED: connection to db-contacts

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const app = require('./app')
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require('./app');
+
+const DB_HOST =
+  'mongodb+srv://dmytrokovach333:jajtGw9A4ZCC2tBG@cluster0.91ksr0k.mongodb.net/db-contacts?retryWrites=true&w=majority';
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log('Database connected success');
+    app.listen(3000, () => {
+      console.log('Server running. Use our API on port: 3000');
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
