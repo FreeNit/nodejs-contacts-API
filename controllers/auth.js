1a55f912906a7457db9f7bdb3f51e18a1257c3e3
Dimon <bolilui.dmytro@gmail.com>
2023-07-11 14:49:38 +0300

    ADDED:  functionality to update user avatar

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,15 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const gravatar = require('gravatar');
+
+const path = require('path');
+const fs = require('fs/promises');
 
 const { User } = require('../models/user');
 
 const { HttpError, ctrlWrapper } = require('../helpers');
+
+const avatarsDir = path.join(__dirname, '../', 'public', 'avatars');
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -14,8 +20,13 @@ const register = async (req, res) => {
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email);
 
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
 
   res.status(201).json({
     email: newUser.email,
@@ -64,8 +75,24 @@ const logout = async (req, res) => {
   });
 };
 
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+  const resultUpload = path.join(avatarsDir, originalname);
+  await fs.rename(tempUpload, resultUpload);
+
+  const avatarURL = path.join('avatars', originalname);
+
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.json({
+    avatarURL,
+  });
+};
+
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
   logout: ctrlWrapper(logout),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const ctrl = require('../../controllers/auth');
 
-const { validation: validateBody, authenticate } = require('../../middlewares');
+const {
+  validation: validateBody,
+  authenticate,
+  upload,
+} = require('../../middlewares');
 
 const { schemas } = require('../../models/user');
 
@@ -20,5 +24,12 @@ router.post('/login', validateBody(schemas.loginSchema), ctrl.login);
 
 // logout
 router.post('/logout', authenticate, ctrl.logout);
+
+router.patch(
+  '/avatars',
+  authenticate,
+  upload.single('avatar'),
+  ctrl.updateAvatar
+);
 
 module.exports = router;
